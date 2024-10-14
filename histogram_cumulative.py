4910d2fdf69cf2b471a794e02f32cd4b6077d7fc
bridlin <bridlinbarckmann@gmail.com>
2024-10-14 15:55:04 +0200

    linecolor, and lablesizes changed

--- histogram_cumulative.py
+++ histogram_cumulative.py
@@ -28,30 +28,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-distance = 'data/distance_ori-PCF-BST_G4hunter_427-2018_sorted.bed'
-distance_shuffled = 'data/distance_ori-shuffled666-PCF-BST_G4hunter_427-2018_sorted.bed'
+distance = 'data/distance_ori-BSF-PCF_ORIs_RNASE_G4-K_927.bed'
+distance_shuffled = 'data/distance_ori_shuffled666-BSF-PCF_ORIs_RNASE_G4-K_927.bed'
 df_distance = pd.read_table(distance,index_col=0)
 df_distance_shuffled = pd.read_table(distance_shuffled,index_col=0)
 df_distance.columns =['distance']
 df_distance = df_distance.assign(type=['ori']*len(df_distance))
 df_distance_shuffled.columns =['distance']
-df_distance_shuffled = df_distance_shuffled.assign(type=['control']*len(df_distance_shuffled))
+df_distance_shuffled = df_distance_shuffled.assign(type=['shuffled']*len(df_distance_shuffled))
 #print(df_distance.head())
 #print(df_distance_shuffled.head())
 frames = [df_distance, df_distance_shuffled]
-print(frames)
+#print(frames)
 distance_all = pd.concat(frames)
-print(distance_all.head())
-fig, ax = plt.subplots(figsize = (10,8))
+#print(distance_all.head())
+fig, ax = plt.subplots(figsize = (12,11))
+
 sns.set_style('darkgrid')
 
 sns.ecdfplot(x=distance_all.distance, data=distance_all, hue='type');
 
-print(distance_all.groupby('type')['distance'].quantile(.8))
+plot = sns.ecdfplot(x=distance_all.distance, data=distance_all, hue='type', palette=['#882255','#44AA99'], linewidth=4);
+plot.axes.set_title("",fontsize=50)
+plot.set_xlabel("distance to closest G4 (bp) ",fontsize=30, labelpad=30)
+plot.set_ylabel("proportion",fontsize=30, labelpad=30)
+plot.tick_params(labelsize=20)
+#plot.legend(title='', loc='lower right', labels=['shuffled control','origins'], fontsize=20)    
+plot.legend_.remove() 
+
+# print the distance at 80th percentile
+print(distance_all.groupby('type')['distance'].quantile(.5))
 
 # control x and y limits
 plt.ylim(0, None)
-plt.xlim(0, 30000)
+plt.xlim(0,30000)
 
 plt.show()
 
@@ -59,39 +69,3 @@ plt.show()
 # axs = fig.subplots(1, 2, sharex=True, sharey=True)
 
 
-# #Cumulative distributions.
-# axs[0].ecdf(df_distance, label="CDF")
-# n, bins, patches = axs[0].hist(df_distance, n_bins, density=True, histtype="step",cumulative=True, label="Cumulative histogram")
-# x = np.linspace(df_distance.min(), df_distance.max())
-# y = ((1 / (np.sqrt(2 * np.pi) * sigma)) *
-#      np.exp(-0.5 * (1 / sigma * (x - mu))**2))
-# y = y.cumsum()
-# y /= y[-1]
-# axs[0].plot(x, y, "k--", linewidth=1.5, label="Theory")
-
-# # Complementary cumulative distributions.
-# axs[1].ecdf(data, complementary=True, label="CCDF")
-# axs[1].hist(data, bins=bins, density=True, histtype="step", cumulative=-1,
-#             label="Reversed cumulative histogram")
-# axs[1].plot(x, 1 - y, "k--", linewidth=1.5, label="Theory")
-
-# Label the figure.
-# fig.suptitle("Cumulative distributions")
-# for ax in axs:
-#     ax.grid(True)
-#     ax.legend()
-#     ax.set_xlabel("Annual rainfall (mm)")
-#     ax.set_ylabel("Probability of occurrence")
-#     ax.label_outer()
-
-#plt.show()
-
-# %%
-#
-# .. admonition:: References
-#
-#    The use of the following functions, methods, classes and modules is shown
-#    in this example:
-#
-#    - `matplotlib.axes.Axes.hist` / `matplotlib.pyplot.hist`
-#    - `matplotlib.axes.Axes.ecdf` / `matplotlib.pyplot.ecdf`
